229d1a640425ec878ed7119b87b92bfc53fd666c
Tom Mason <wheybags@wheybags.com>
2014-01-26 17:03:17 +0000

    added special random functions

---
+++ apps/freeablo/level/random.h
@@ -0,0 +1,11 @@
+#ifndef FA_RANDOM_H
+#define FA_RANDOM_H
+
+namespace Freeablo
+{
+    void FAsrand(int seed);
+    int normRand(int min, int max);
+    int randomInRange (unsigned int min, unsigned int max);
+}
+
+#endif
