138ab6d0257595c706e65329bd1264846a4de02a
Tom Mason <wheybags@wheybags.com>
2014-02-09 14:03:22 +0000

    doxygen

---
+++ components/cel/doc.h
@@ -0,0 +1,8 @@
+// Note: This is not a regular source file
+
+/// \ingroup components
+/// \defgroup cel Cel
+
+/// \namespace Cel
+/// \ingroup cel
+/// \brief Library for loading blizzard CEL and CL2 files
---
+++ components/misc/doc.h
@@ -0,0 +1,8 @@
+// Note: This is not a regular source file
+
+/// \ingroup components
+/// \defgroup misc Misc
+
+/// \namespace Misc
+/// \ingroup misc
+/// \brief miscellaneous stuff
