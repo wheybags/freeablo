cad0ae956c3e7c8738cecde1fc62f664abf83e2b
Tom Mason <wheybags@wheybags.com>
2015-05-12 20:41:44 +0100

    add support for playing sound samples

---
+++ apps/freeablo/faaudio/audiomanager.h
@@ -0,0 +1,47 @@
+#ifndef AUDIO_MANAGER_H
+#define AUDIO_MANAGER_H
+
+#include <stdint.h>
+
+#include <string>
+#include <vector>
+#include <map>
+#include <list>
+
+#include <audio/audio.h>
+
+namespace FAAudio
+{
+    struct CacheEntry
+    {
+        Audio::Sound* sound;
+        std::list<std::string>::iterator usedListIt;
+
+        CacheEntry(Audio::Sound* _sound, std::list<std::string>::iterator _usedListIt)
+            : sound(_sound), usedListIt(_usedListIt) {}
+
+        CacheEntry() {}
+    };
+
+    class AudioManager
+    {
+        public:
+            AudioManager(int32_t channelCount, size_t cacheSize);
+            ~AudioManager();
+
+            void play(std::string path);
+            void playMusic(const std::string& path);
+
+        private:
+            std::vector<std::string> mPlaying;
+            std::map<std::string, CacheEntry> mCache;
+            std::list<std::string> mUsedList;
+
+            size_t mCacheSize;
+            size_t mCount;
+
+            Audio::Music* mCurrentMusic;
+    };
+}
+
+#endif
--- components/audio/audio.h
+++ components/audio/audio.h
@@ -3,15 +3,25 @@
 
 #include <string>
 
+#include <stdint.h>
+
 namespace Audio
 {
-    void init();
+    void init(size_t channelCount);
     void quit();
-    
+
     typedef void Music;
+    typedef void Sound;
+
     Music* loadMusic(const std::string& path);
     void freeMusic(Music* mus);
     void playMusic(Music* mus);
+
+    Sound* loadSound(const std::string& path);
+    void freeSound(Sound* sound);
+    int32_t playSound(Sound* sound);
+
+    bool channelPlaying(int32_t channel);
 }
 
 #endif
