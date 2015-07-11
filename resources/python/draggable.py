bb1ee9090e91e47baba80d14d87af2cbdd4ae5dc
Colm Vize <vizec@tcd.ie>
2015-07-11 15:54:28 +0100

    DraggableWidget no longer inherits from DocManage

--- resources/python/draggable.py
+++ resources/python/draggable.py
@@ -2,11 +2,7 @@
 from docmanage import DocManager
 import inspect
 import pprint
-class DraggableWidget(DocManager):
-
-
-
-
+class DraggableWidget:
     def __init__(self, document, xoffset, yoffset):
         self.document = document
         self.xoffset = xoffset
@@ -18,20 +14,19 @@ class DraggableWidget(DocManager):
         my = event.parameters['mouse_y']
         event.current_element.style.left= str(mx+self.xoffset)
         event.current_element.style.top = str(my+self.yoffset)
-        print " (%s, %s)" % (event.current_element.style.left,
-            event.current_element.style.top)
+        #print " (%s, %s)" % (event.current_element.style.left,
+        #    event.current_element.style.top)
 
     def guiMouseDown(self, event):
         pass
         #print "guiMouseDown called: %s" % (event.current_element)
         #for attr in inspect.getmembers(event.current_element):
         #    print attr[0]
+
     def bodyMouseDown(self, event):
 
         print "bodyMouseDown called: %s" % (event.current_element)
-        for attr in inspect.getmembers(event):
-            print "%s : %s" % (attr[0], attr[1])
+        #for attr in inspect.getmembers(event):
+        #    print "%s : %s" % (attr[0], attr[1])
     def bodyKeyDown(self, event):
         print "bodyKeyDown called: %s" % (event.current_element)
-
-
