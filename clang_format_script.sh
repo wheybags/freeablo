7c37d0da86a988ef044f9b5ee70183e7331fd044
Tom Mason <wheybags@wheybags.com>
2020-02-12 15:37:09 +0100

    allow running clang-format locally
    only do this if you have the same exact version as the server!

--- clang_format_script.sh
+++ clang_format_script.sh
@@ -8,17 +8,19 @@ for x in $paths; do
     find $x -name *.h -o -name *.cpp | xargs clang-format -i -style=file
 done
 
-DIFF_FILE=/tmp/fa_format_diff.patch
+if [ ! -z "$TRAVIS" ]; then
+    DIFF_FILE=/tmp/fa_format_diff.patch
 
-git diff > $DIFF_FILE
+    git diff > $DIFF_FILE
 
-if [ -s $DIFF_FILE ]; then
-    DIFF_URL=$(cat "$DIFF_FILE" | nc termbin.com 9999 | tr -d '\0')
-    echo -e "\e[31m" # red
-    echo "Autoformat needed, to apply the formatting changes locally, run:"
-    echo "curl $DIFF_URL > $DIFF_FILE && git apply $DIFF_FILE"
-    echo -e "\e[0m" # end red
-    exit 1
+    if [ -s $DIFF_FILE ]; then
+        DIFF_URL=$(cat "$DIFF_FILE" | nc termbin.com 9999 | tr -d '\0')
+        echo -e "\e[31m" # red
+        echo "Autoformat needed, to apply the formatting changes locally, run:"
+        echo "curl $DIFF_URL > $DIFF_FILE && git apply $DIFF_FILE"
+        echo -e "\e[0m" # end red
+        exit 1
+    fi
 fi
 
 exit 0
