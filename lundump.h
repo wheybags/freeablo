64eaccc27142d156ffdeba4cfdced59d61d2b5d8
Jordy Vieira <jordyvieira@hotmail.com>
2020-02-21 11:15:12 -0300

    Squashed 'extern/lua/' content from commit 9b7987a9d
    git-subtree-dir: extern/lua
    git-subtree-split: 9b7987a9d1471ba94764286b28e0998f73deb46a

---
+++ lundump.h
@@ -0,0 +1,31 @@
+/*
+** $Id: lundump.h $
+** load precompiled Lua chunks
+** See Copyright Notice in lua.h
+*/
+
+#ifndef lundump_h
+#define lundump_h
+
+#include "llimits.h"
+#include "lobject.h"
+#include "lzio.h"
+
+
+/* data to catch conversion errors */
+#define LUAC_DATA	"\x19\x93\r\n\x1a\n"
+
+#define LUAC_INT	0x5678
+#define LUAC_NUM	cast_num(370.5)
+
+#define LUAC_VERSION	LUA_VERSION_NUM
+#define LUAC_FORMAT	0	/* this is the official format */
+
+/* load one chunk; from lundump.c */
+LUAI_FUNC LClosure* luaU_undump (lua_State* L, ZIO* Z, const char* name);
+
+/* dump one chunk; from ldump.c */
+LUAI_FUNC int luaU_dump (lua_State* L, const Proto* f, lua_Writer w,
+                         void* data, int strip);
+
+#endif
