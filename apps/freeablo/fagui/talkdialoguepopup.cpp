8e1b9a792cc36c116b23843f687312c53f8534cd
neveza <neveza@aol.com>
2020-07-01 20:25:56 -0400

    Update talkdialoguepopup.cpp
    Trying to fix the format

--- apps/freeablo/fagui/talkdialoguepopup.cpp
+++ apps/freeablo/fagui/talkdialoguepopup.cpp
@@ -27,39 +27,41 @@ namespace FAGui
             ctx, "talkPopup", dialogRectangle, flags, boxTex->getNkImage(), [&]() { drawBackgroundCheckerboard(renderer, ctx, dialogRectangle); }, true);
 
         // creating another window allows for texts to scroll without going out of the textured background frame
-        mGuiManager.nk_fa_begin_window(ctx,
-                                       "textScroll",
-                                        textScrollRectangle,
-                                        flags,
-                                        [&]() {
-                                            // fill the rest of the window
-                                            struct nk_rect bounds = nk_widget_bounds(ctx);
-                                            struct nk_rect panelSize = nk_window_get_bounds(ctx);
-                                            float contentHeight = panelSize.h + panelSize.y - bounds.y;
-                                            nk_layout_row_dynamic(ctx, contentHeight, 1);
+        mGuiManager.nk_fa_begin_window(
+            ctx,
+            "textScroll",
+            textScrollRectangle,
+            flags,
+            [&]() {
+                // fill the rest of the window
+                struct nk_rect bounds = nk_widget_bounds(ctx);
+                struct nk_rect panelSize = nk_window_get_bounds(ctx);
+                float contentHeight = panelSize.h + panelSize.y - bounds.y;
+                nk_layout_row_dynamic(ctx, contentHeight, 1);
 
-                                            ctx->style.window.fixed_background.data.color = nk_color{0, 0, 0, 0};
+                ctx->style.window.fixed_background.data.color = nk_color{0, 0, 0, 0};
 
-                                            auto wrapText = [](nk_context* ctx, const char* text, TextColor color) {
-                                                FARender::Renderer* renderer = FARender::Renderer::get();
-                                                nk_style_push_font(ctx, renderer->bigTGoldFont());
-                                                nk_style_push_color(ctx, &ctx->style.text.color, getNkColor(color));
-                                                nk_label_wrap(ctx, text);
-                                                nk_style_pop_color(ctx);
-                                                nk_style_pop_font(ctx);
-                                            };
+                auto wrapText = [](nk_context* ctx, const char* text, TextColor color) {
+                    FARender::Renderer* renderer = FARender::Renderer::get();
+                    nk_style_push_font(ctx, renderer->bigTGoldFont());
+                    nk_style_push_color(ctx, &ctx->style.text.color, getNkColor(color));
+                    nk_label_wrap(ctx, text);
+                    nk_style_pop_color(ctx);
+                    nk_style_pop_font(ctx);
+                };
 
-                                            auto& world = mGuiManager.mDialogManager.mWorld;
-                                            static auto startTime = world.getCurrentTick();
-                                            wrapText(ctx, mTalkData.text.c_str(), TextColor::white);
-                                            auto currentTime = world.getCurrentTick();
-                                            if (currentTime - startTime >= world.getTicksInPeriod("0.1"))
-                                            {
-                                                ctx->active->scrollbar.y++;
-                                                startTime = currentTime;
-                                            }
-                                        },
-                                        true);
+                auto& world = mGuiManager.mDialogManager.mWorld;
+                static auto startTime = world.getCurrentTick();
+                wrapText(ctx, mTalkData.text.c_str(), TextColor::white);
+                auto currentTime = world.getCurrentTick();
+                if (currentTime - startTime >= world.getTicksInPeriod("0.1"))
+                {
+                    ctx->active->scrollbar.y++;
+                    startTime = currentTime;
+                }
+            },
+            true);
+
         return result;
     }
 
